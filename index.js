5f7c699c4f0ac57f224098b44fb0872f01f6a313
vaibhav0726 <vaibhav.varshney2_cs20@gla.ac.in>
2022-11-08 00:51:56 +0530

    linked pages from server

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-
+require('dotenv').config();
 const express = require('express');
+const path = require('path'); // inbuilt in node 
 const port = process.env.PORT || 3000;
 const app = express();
+const router = express.Router();
+
+app.use( express.static( "views" ));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', function(req, res){
-    console.log(req);
-    res.send('<h1>cool, it is running!</h1>'); 
+    // console.log(req);
+    // console.log(__dirname); // directory where index.js is located and started
+    // res.send('<h1>it is running!</h1>');
+    return res.render('resources');
+}) 
+
+app.get('/temp', function(req, res){
+    return res.render('temp');
 }) 
 
 app.listen(port, function(err){
